f8556e6ead1755eddf41b6aeb2c8b56a2ae7650b
YLIssakha235 <yayalibis05@gmail.com>
2026-09-25 10:24:56 +0200

    WS2 POI data prefab and editor manage end

---
+++ Assets/Workshop2-VisiteVirtuelle/Scipts/EditorWindow.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using TMPro;
+
+public class EditorWindow : MonoBehaviour
+{
+    public EditorManager EditorManager;
+    public TMP_InputField TitleInputField, ContentTextField;
+
+    private PointOfInterestStruct _poiInfo;
+
+    public void FillData(PointOfInterestStruct poiInfo)
+    {
+        this._poiInfo = poiInfo;
+        TitleInputField.text = poiInfo.Title;
+        ContentTextField.text = poiInfo.Description;
+    }
+
+    public void SavePOI()
+    {
+        _poiInfo.Title = TitleInputField.text;
+        _poiInfo.Description = ContentTextField.text;
+
+        EditorManager.SavePointOfInterest(_poiInfo);
+    }
+
+    public void DeletePOI()
+    {
+        EditorManager.DeletePointOfInterest(_poiInfo.Id);
+    }
+}
